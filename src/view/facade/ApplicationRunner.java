3910f7e427a3e7c9a28202e1374c67d6ae17a335
Irina.Constantinova <irina.constantinova@gmail.com>
2024-03-20 16:32:23 +0300

    Added logger

--- src/view/facade/ApplicationRunner.java
+++ src/view/facade/ApplicationRunner.java
@@ -1,11 +1,19 @@
 package view.facade;
 
 import controller.ComplexCalculatorController;
+import logger.Log;
 import model.repository.impl.ComplexCalculator;
 import view.ComplexCalculatorView;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ApplicationRunner {
+
+    private static final Logger log = Log.log(ApplicationRunner.class.getName());
+
     public static void run(){
+        log.log(Level.INFO, "Method run started");
         ComplexCalculator calculator = new ComplexCalculator();
         ComplexCalculatorView view = new ComplexCalculatorView();
         ComplexCalculatorController controller = new ComplexCalculatorController(view, calculator);
